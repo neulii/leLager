b0f1ed58d00bfbb39d347dfe97d3913dd4961fed
neulii <neulii@gmx.at>
2021-01-06 11:11:13 +0100

    changes made in osx

---
+++ leLager/include/inputReportWindow.h
@@ -0,0 +1,22 @@
+#ifndef INPUTREPORTWINDOW_H
+#define INPUTREPORTWINDOW_H
+
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+class InputReportWindow : public wxFrame
+{
+
+    public:
+        InputReportWindow(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+
+
+
+
+};
+
+#endif // !INPUTREPORTWINDOW_H
---
+++ leLager/src/inputReportWindow.cpp
@@ -0,0 +1,10 @@
+#include "inputReportWindow.h"
+
+
+
+ InputReportWindow::InputReportWindow(const wxString& title, const wxPoint& pos, const wxSize& size)
+    :wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+
+
+}
